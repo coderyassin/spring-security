18d6e7f3d5fa2becfc0489e72394237b713d2eae
coderyassin <yassin.mellouki.j@gmail.com>
2022-09-20 00:16:05 +0100

    Add statics files #2

---
+++ src/main/java/org/yascode/springsecurity/controller/MainController.java
@@ -0,0 +1,30 @@
+package org.yascode.springsecurity.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("api/")
+public class MainController {
+
+    @GetMapping("main")
+    public String index(){
+        return "index";
+    }
+
+    @GetMapping("profile")
+    public String profile(){
+        return "profile/index";
+    }
+
+    @GetMapping("management")
+    public String management(){
+        return "management/index";
+    }
+
+    @GetMapping("admin")
+    public String admin(){
+        return "admin/index";
+    }
+}
