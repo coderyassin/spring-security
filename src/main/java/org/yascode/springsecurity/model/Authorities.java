c8566d26b8a61e4f5ff5e807a2e0f193de4b9836
coderyassin <yassin.mellouki.j@gmail.com>
2022-09-25 22:55:11 +0100

    Create model (User - Role - Authorities) #9

---
+++ src/main/java/org/yascode/springsecurity/model/Authorities.java
@@ -0,0 +1,13 @@
+package org.yascode.springsecurity.model;
+
+import java.util.Set;
+
+public class Authorities {
+
+    private Long id;
+
+    private String authorityName;
+
+    private Set<User> users;
+
+}
---
+++ src/main/java/org/yascode/springsecurity/model/Role.java
@@ -0,0 +1,12 @@
+package org.yascode.springsecurity.model;
+
+import java.util.Set;
+
+public class Role {
+
+    private Long id;
+
+    private String roleName;
+
+    private Set<User> users;
+}
