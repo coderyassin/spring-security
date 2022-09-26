3e7b48daafcd37f72088ff0bdc94d118e5d3377d
coderyassin <yassin.mellouki.j@gmail.com>
2022-09-26 19:05:18 +0100

    UserPrincipalDetailsService && UserPrincipal #15

---
+++ src/main/java/org/yascode/springsecurity/model/UserPrincipal.java
@@ -0,0 +1,60 @@
+package org.yascode.springsecurity.model;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+
+@RequiredArgsConstructor
+public class UserPrincipal implements UserDetails {
+
+    private final User user;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        List<GrantedAuthority> authorities = new ArrayList<>();
+        this.user.getAuthorities().forEach(p -> {
+            GrantedAuthority authority = new SimpleGrantedAuthority(p.getAuthorityName());
+            authorities.add(authority);
+        });
+        /*this.user.getRoles().forEach(p -> {
+            GrantedAuthority authority = new SimpleGrantedAuthority("ROLE_" + p.getRoleName());
+            authorities.add(authority);
+        });*/
+        return authorities;
+    }
+
+    @Override
+    public String getPassword() {
+        return this.user.getPassword();
+    }
+
+    @Override
+    public String getUsername() {
+        return this.user.getUserName();
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return this.user.getActive() == 1;
+    }
+}
---
+++ src/main/java/org/yascode/springsecurity/securityconfig/UserPrincipalDetailsService.java
@@ -0,0 +1,22 @@
+package org.yascode.springsecurity.securityconfig;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.yascode.springsecurity.dao.UserRepository;
+import org.yascode.springsecurity.model.User;
+import org.yascode.springsecurity.model.UserPrincipal;
+
+@RequiredArgsConstructor
+public class UserPrincipalDetailsService implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = userRepository.findByUserName(username).get();
+        UserPrincipal userPrincipal = new UserPrincipal(user);
+        return userPrincipal;
+    }
+}
