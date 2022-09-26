bf57e27e41d1558ef1f26836c4101d7849676d18
coderyassin <yassin.mellouki.j@gmail.com>
2022-09-26 23:06:49 +0100

    Logout SecurityConfiguration #19

--- src/main/java/org/yascode/springsecurity/securityconfig/SecurityConfiguration.java
+++ src/main/java/org/yascode/springsecurity/securityconfig/SecurityConfiguration.java
@@ -10,6 +10,7 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 @Configuration
 @EnableWebSecurity
@@ -38,7 +39,8 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
                 .loginPage("/api/login")
                 .usernameParameter("user")
                 .passwordParameter("pass")
-                .permitAll();
+                .and()
+                .logout().logoutRequestMatcher(new AntPathRequestMatcher("/logout")).logoutSuccessUrl("/api/login");
     }
 
     @Bean
